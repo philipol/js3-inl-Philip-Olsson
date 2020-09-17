da8ad2e9e115ae0c5e4e5cf5a6bf6a9391ee4f95
Hassan Mian <hassanmian@hassans-imac.lan>
2020-09-17 10:28:15 +0200

    Add register to UserKit

--- src/data/UserKit.js
+++ src/data/UserKit.js
@@ -2,3 +2,15 @@ const ROOT_URL = 'https://frebi.willandskill.eu/'
 const API_URL = `${ROOT_URL}api/v1/`
 const AUTH_URL = `${ROOT_URL}auth/`
 const LOGIN_URL = `${ROOT_URL}api-token-auth/`
+export default class {
+  async register(firstName, lastName, email, password, organisationName, organisationKind) {
+    const url = `${AUTH_URL}users/`
+    const payload = { firstName, lastName, email, password, organisationName, organisationKind }
+    return fetch(url, {
+      method: "POST",
+      headers: this.getPublicHeaders(),
+      body: JSON.stringify(payload)
+    })
+  }
+
+}
