179cf031656cef789894d893fe090baa1099f443
Hassan Mian <hassanmian@hassans-imac.lan>
2020-09-17 10:28:30 +0200

    Handle Create User

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import {Switch, Route, useHistory} from 'react-router-dom'
 import './App.css';
 
 function App() {
+  function handleCreateUser() {
+    userKit.register("Hassan", "Mian", "mian+test+4@willandskill.se", "hej123svejs43321", "Mitt företag AB", "0")
+  }
   return (
       <Switch>
           Edit <code>src/App.js</code> and save to reload.
