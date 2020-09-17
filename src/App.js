e3c673e0be5ec898a27a85696ebeafca0996c933
Hassan Mian <hassanmian@hassans-imac.lan>
2020-09-17 10:26:10 +0200

    Import react-router-dom dependencies and create switch and first route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import {Switch, Route, useHistory} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      <Switch>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Route path="/">
+          <h1>Register</h1>
+          <button onClick={handleCreateUser}>Create User</button>
+        </Route>
+      </Switch>
     </div>
   );
 }
